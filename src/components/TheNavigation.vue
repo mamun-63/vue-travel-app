e0795ed79e7f2dce84c9088020ccd72b3d7d9421
Mamun <mamun.cse63@gmail.com>
2020-11-17 18:16:11 +0600

    rerender components when vue router params changes

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,27 +1,63 @@
 <template>
   <nav id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/panama">Panama</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to='/'>Home</router-link>
+      </li>
+      <li v-for="destination in destinations" :key="destination.name" class="links">
+        <router-link :to="{name: 'DestinationDetails', params: {id: destination.id}}">
+          {{ destination.name }}
+        </router-link>
+      </li>
+    </ul>
   </nav>
 </template>
 
+<script>
+import store from '../store'
+
+export default {
+  data() {
+    return {
+      destinationId: this.$route.params.id,
+      destinations: store.destinations
+    }
+  }
+}
+</script>
+
 <style scoped>
-#nav {
-  display: flex;
-  align-items: center;
-  padding: 30px;
-}
+  #nav {
+    display: flex;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  padding: 0 10px;
-}
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+  }
 
-#nav a.vue-router-active-class {
-  color: purple;
-}
+  #nav a.vue-router-active-class {
+    color: purple;
+  }
+
+  .nav-links {
+    display: flex;
+  }
+
+  .links {
+    padding-right: 20px;
+    list-style: none;
+  }
+
+  .links:hover {
+    text-decoration: none;
+  }
+
+  .logo {
+    font-size: 20px;
+    color: purple;
+    font-weight: bold;
+  }
+
 </style>
