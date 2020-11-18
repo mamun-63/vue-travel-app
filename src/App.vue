df9114421749ebe6e88f0cd7ddf8f753557ed76d
Mamun <mamun.cse63@gmail.com>
2020-11-19 01:18:04 +0600

    route transition class

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <TheNavigation />
-    <router-view :key="$route.path" />
+    <!-- name="slide" mode="out-in" -->
+    <!-- name="moveUp" -->
+    <transition name="fade">
+      <router-view :key="$route.path" />
+    </transition>
   </div>
 </template>
 
@@ -23,4 +27,51 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
+/* .slide-enter-active,
+.slide-leave-active {
+  transition: opacity 1s, transform 1s;
+}
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+} */
+
+
+/* .moveUp-enter-active {
+  animation: fadein 1s ease-in ;
+}
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.moveUp-leave-active {
+  animation: moveUp 0.3s ease-in;
+}
+@keyframes moveUp {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-400px);
+  }
+} */
+
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+} 
 </style>
