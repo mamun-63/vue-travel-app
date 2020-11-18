52b4cedd2b017f39d3bebe1f5f8ce285148fc49a
Mamun <mamun.cse63@gmail.com>
2020-11-19 00:26:24 +0600

    nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -23,13 +23,24 @@ const routes = [
     props: true,
   },
   {
-    path: '/details/:slug',
+    path: '/destinaton/:slug',
     name: 'DestinationDetails',
+    props: true,
     component: () =>
       import(
         /* webpackChunkName: "DestinationDetails" */ '../views/DestinationDetails.vue'
       ),
-    props: true,
+    children: [
+      {
+        path: ':experienceSlug',
+        name: 'experienceDetails',
+        props: true,
+        component: () =>
+          import(
+            /* webpackChunkName: "ExperienceDetails" */ '../views/ExperienceDetails.vue'
+          ),
+      },
+    ],
   },
 ]
 
--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,14 +1,28 @@
 <template>
-  <section class="destination">
-    <h1>{{ destination.name }}</h1>
-    <div class="destination-details">
-      <img
-        :src="require(`../assets/${destination.image}`)"
-        :alt="destination.name"
-      />
-      <p>{{ destination.description }}</p>
-    </div>
-  </section>
+  <div>
+    <section class="destination">
+      <h1>{{ destination.name }}</h1>
+      <div class="destination-details">
+        <img
+          :src="require(`../assets/${destination.image}`)"
+          :alt="destination.name"
+        />
+        <p>{{ destination.description }}</p>
+      </div>
+    </section>
+    <section class="experiences">
+      <h2>Top experiences in {{ destination.name }} </h2>
+      <div class="cards">
+        <div v-for="experience in destination.experiences" :key="experience.slug" class="card">
+          <router-link :to="{name: 'experienceDetails', params: {experienceSlug: experience.slug} }">
+            <img :src="require(`../assets/${experience.image}`)" :alt="experience.name">
+            <span class="card__text">{{ experience.name }}</span>
+          </router-link>
+        </div>
+      </div>
+      <router-view :key="$route.path"/>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -39,13 +53,13 @@ export default {
 
 <style scoped>
 img {
-  max-width: 600px;
+  max-width: 500px;
   max-height: 400px;
   height: 300px;
   object-fit: contain;
 }
 
-.destination-details {
+.destination-details, .cards {
   display: flex;
   justify-content: space-between;
 }
@@ -54,5 +68,25 @@ p {
   margin: 0 40px;
   font-size: 20px;
   text-align: left;
+}
+
+.cards img {
+  max-height: 200px;
+}
+
+.card {
+  padding: 0 20px;
+  position: relative;
+}
+
+.card__text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 25px;
+  font-weight: bold;
+  text-decoration: none;
 }
 </style>
